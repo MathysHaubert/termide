b1400585e2aeadca23b6b86a86010e612e3dddae
Nick Nizovtsev <nizovtsevnv@gmail.com>
2026-09-13 23:42:15 +0400

    chore: release version 0.35.0
    Major changes:
    - Image viewer zoom with +/-, mouse wheel, and status bar buttons
    - Shell completions for bash, zsh, and fish
    - Dynamic line-number gutter width
    - Enter and double-click open files in Git Status
    - Operations panel sizes to its cards
    - Recursive FSEvents watch on macOS
    - Emoji width agreement with host terminal
    - Wide character grid understanding in terminal

--- flake.nix
+++ flake.nix
@@ -81,7 +81,7 @@
         packages = {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "termide";
-            version = "0.34.1";
+            version = "0.35.0";
 
             src = ./.;
 
@@ -114,7 +114,7 @@
             muslPkgs = pkgs.pkgsCross.musl64;
           in muslPkgs.rustPlatform.buildRustPackage {
             pname = "termide";
-            version = "0.34.1";
+            version = "0.35.0";
 
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
